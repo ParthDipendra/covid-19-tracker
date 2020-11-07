c6dc9b4183fbfad3522dd8ad573105cb73ae21ed
Rishabh Amga <rishabhamga@Rishabhs-MacBook-Pro.local>
2020-11-07 21:58:30 +0530

    final commit

--- src/Map.js
+++ src/Map.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { MapContainer as LeafletMap, TileLayer } from "react-leaflet";
+import { Map as LeafletMap, TileLayer } from "react-leaflet";
 import "./Map.css";
 import { showDataOnMap } from "./util";
 
